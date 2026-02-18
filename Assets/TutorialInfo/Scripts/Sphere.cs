64a78fd5c95746ac520344451e6c0d6f9a7833c6
Ryo-suzusan <ryo.ohta1017@gmail.com>
2026-02-18 17:15:23 +0900

    sphereクラスの作成

---
+++ Assets/TutorialInfo/Scripts/Sphere.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    [SerializeField] Vector3 position = new Vector3(0, 0, 0);
+    [SerializeField] float radius = 0;
+
+    [SerializeField] Vector3 speed = new Vector3(1, 1, 1);
+
+    public Sphere(Vector3 position, float radius)
+    {
+        this.position = position;
+        this.radius = radius;
+    }
+
+    public void Simulate()
+    {
+        position += speed;
+    }
+}
